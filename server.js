08be5cdd5cd726d2b88b5ffb75dbbaf0ce0d22e0
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-05-06 12:07:22 -0500

    added html paths

--- server.js
+++ server.js
@@ -9,13 +9,21 @@ app.use(express.json());
 app.use(express.static("public"));
 const notes = require("./db/db.json");
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 app.get("/api/notes", (req, res) => {
     res.json(notes);
 });
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.post("/api/notes", (req, res) => {
     req.body.id = uuidv4();
